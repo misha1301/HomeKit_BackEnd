14d92ae6db5c9a7cee4fc825d9970e80d80121a5
Misha Oliinyk <380660775646a@gmail.com>
2024-05-13 23:21:55 +0300

    change bcrypt to bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,52 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 //require('dotenv').config();
 
 const handleLogin = async (req, res) => {
-    const {username, password} = req.body;
-    if(!username || !password) 
-    return res.status(400).json({"message": "Username and password are reqiured."});
-    // check for duplicate usernames in the db
-    const foundUser = await User.findOne({username: username}).exec();
-    if(!foundUser)
-    return res.sendStatus(401); //Unauthorized
-    //evaluate passwors
-    const match = await bcrypt.compare(password, foundUser.password);
-    if(match){
-        const roles = Object.values(foundUser.roles);
-        //create JWTs
-        const accessToken = jwt.sign(
-            {
-                "UserInfo":{
-                    "username": foundUser.username,
-                    "roles": roles
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: '60s'}
-        );
-        const refreshToken = jwt.sign(
-            {"username": foundUser.username},
-            process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: '1d'}
-        );
-        //saving refresh token with current user
-        foundUser.refreshToken = refreshToken;
-        const result = await foundUser.save();
-        console.log(result);
-        
-        res.cookie("jwt", refreshToken, {httpOnly: true, sameSite: 'None', secure: true,  maxAge: 24*60*60*1000}); //secure: true,
-        res.json({ roles, accessToken });
-    } else{
-        res.sendStatus(401);
-    }
-}
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "Username and password are reqiured." });
+  // check for duplicate usernames in the db
+  const foundUser = await User.findOne({ username: username }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  //evaluate passwors
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    const roles = Object.values(foundUser.roles);
+    //create JWTs
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "60s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    //saving refresh token with current user
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
 
-module.exports = {handleLogin};
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    }); //secure: true,
+    res.json({ roles, accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,30 +1,31 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 const handleNewUser = async (req, res) => {
-    const {username, password} = req.body;
-    if(!username || !password) 
-    return res.status(400).json({"message": "Username and password are reqiured."});
-    
-    // check for duplicate usernames in the db
-    const duplicate = await User.findOne({username: username}).exec();
-    if(duplicate)
-    return res.sendStatus(409);
-    try {
-        //encrypt the password
-        const hashPwd = await bcrypt.hash(password, 10);
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "Username and password are reqiured." });
 
-        //create and store the new user
-        const result = await User.create({
-            "username": username,
-            "password": hashPwd
-        });
-        console.log(result);
+  // check for duplicate usernames in the db
+  const duplicate = await User.findOne({ username: username }).exec();
+  if (duplicate) return res.sendStatus(409);
+  try {
+    //encrypt the password
+    const hashPwd = await bcrypt.hash(password, 10);
 
-        res.status(201).json({'success':`New user ${username} created`});
-    } catch (err) {
-        res.status(500).json({"message": err.message});
-    }
-}
+    //create and store the new user
+    const result = await User.create({
+      username: username,
+      password: hashPwd,
+    });
+    console.log(result);
 
-module.exports = {handleNewUser};
+    res.status(201).json({ success: `New user ${username} created` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { handleNewUser };
